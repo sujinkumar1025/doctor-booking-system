f845fb4c725f5ccadb63039ddd72a24aaf0f384b
sujinkumar1025 <sujin123>
2025-06-13 06:51:34 +0530

    Add routing config for Vercel

---
+++ vit.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  base: '/', // Make sure this matches your repo name
+  plugins: [react()],
+});
